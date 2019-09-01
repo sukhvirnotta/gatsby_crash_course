fec48be1f632285f27d746a6acf7bbf4cfe9fba1
Sukhvir Notta <s.s.notta@se16.qmul.ac.uk>
2019-09-01 19:56:54 +0100

    Initial commit

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+
+const AboutPage = () => (
+    <Layout>
+    <SEO title="About" />
+    <h1>About Us</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod distinctio sit molestias nostrum veniam tempore explicabo dolore delectus est voluptates laudantium odit dignissimos illum, a dolor eaque asperiores. Dolorum, neque.</p>
+  </Layout>
+)
+
+export default AboutPage;
---
+++ src/pages/services.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+
+const ServicesPage = () => (
+    <Layout>
+    <SEO title="Services" />
+    <h1>Services</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod distinctio sit molestias nostrum veniam tempore explicabo dolore delectus est voluptates laudantium odit dignissimos illum, a dolor eaque asperiores. Dolorum, neque.</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod distinctio sit molestias nostrum veniam tempore explicabo dolore delectus est voluptates laudantium odit dignissimos illum, a dolor eaque asperiores. Dolorum, neque.</p>
+  </Layout>
+)
+
+export default ServicesPage;
